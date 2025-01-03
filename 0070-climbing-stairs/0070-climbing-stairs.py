0b1a6f3a4996d7f58b57fbfc31c04db7651f18ca
Ashriel Nhembo <ashrielnhembo.dev@gmail.com>
2025-01-03 19:03:00 +0530

    Time: 0 ms (100%), Space: 17.7 MB (12.68%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if  n == 1:
+            return 1
+        if n == 2:
+             return 2
+
+        two_back = 1
+        one_back = 2
+        for i in range(2, n):
+            next_num = two_back + one_back
+            two_back = one_back
+            one_back = next_num 
+        return one_back
